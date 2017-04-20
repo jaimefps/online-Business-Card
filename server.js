8d9d2136852d219ff49ad3943898cb481cbdb766
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-04-20 00:42:15 -0700

    corrected styling for computer screens

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, './index.html')));
 
 app.listen(PORT, () => console.log('SERVER ON PORT ', PORT));
